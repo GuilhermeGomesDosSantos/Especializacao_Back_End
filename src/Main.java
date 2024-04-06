f323b6def5598ab0b7bf6da5c7f350c01255dfd4
GuilhermeGomesDosSantos <gomes3654gui@gmail.com>
2024-04-06 17:34:05 -0300

    Atribuindo valores ao filme

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Esse é o Screen Match");
+        System.out.println(("Filme: Top Gun: Maverick"));
+
+        int anoDeLancamentoFilme = 2022;
+
+        System.out.println("Ano de Lançamento: " +anoDeLancamentoFilme);
+
+        boolean incluidoNoPlano = true;
+        double notaFilme = 8.1;
     }
 }
