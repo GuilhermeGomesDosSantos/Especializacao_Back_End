fb38cabceb9db271cd2a52102ae1cdd27a1245c3
GuilhermeGomesDosSantos <gomes3654gui@gmail.com>
2024-04-12 20:46:16 -0300

    Concatenando variavel do tipo char e String

---
+++ src/Atividades/Mensagem/Mensagem.java
@@ -0,0 +1,13 @@
+/*
+Declare uma variável do tipo char (letra) e uma variável do tipo String (palavra). Atribua valores a essas variáveis e concatene-as em uma mensagem.
+ */
+package Atividades.Mensagem;
+
+public class Mensagem {
+    public static void main(String[] args) {
+        char valorChar = 'G';
+        String valorString = "Hello, ";
+
+        System.out.println(valorString + valorChar);
+    }
+}
